4617d72acfeec5b83e01d6f11b71540939b7b8e8
Szymon <szymonjag2@gmail.com>
2023-04-04 11:22:55 +0200

    added validator for adding recipe tags

---
+++ CookLib.ApplicationServices/API/Domain/Validators/RecipeTags/AddRecipeTagValidator.cs
@@ -0,0 +1,14 @@
+﻿using CookLib.ApplicationServices.API.Domain.Requests.RecipeTags;
+using FluentValidation;
+
+namespace CookLib.ApplicationServices.API.Domain.Validators.RecipeTags
+{
+    public class AddRecipeTagValidator : AbstractValidator<AddRecipeTagRequest>
+    {
+        public AddRecipeTagValidator()
+        {
+            RuleFor(x => x.TagId).NotEmpty();
+            RuleFor(x => x.RecipeId).NotEmpty();
+        }
+    }
+}
