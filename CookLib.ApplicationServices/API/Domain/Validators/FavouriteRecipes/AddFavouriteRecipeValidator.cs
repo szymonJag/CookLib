c311d4d7664c264c736e4bf93cff006ee0b477fe
Szymon <szymonjag2@gmail.com>
2023-04-04 10:57:58 +0200

    added validation for adding favourite recipes

---
+++ CookLib.ApplicationServices/API/Domain/Validators/FavouriteRecipes/AddFavouriteRecipeValidator.cs
@@ -0,0 +1,14 @@
+﻿using CookLib.ApplicationServices.API.Domain.Requests.FavouriteRecipes;
+using FluentValidation;
+
+namespace CookLib.ApplicationServices.API.Domain.Validators.FavouriteRecipes
+{
+    public class AddFavouriteRecipeValidator : AbstractValidator<AddFavouriteRecipeRequest>
+    {
+        public AddFavouriteRecipeValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty();
+            RuleFor(x => x.RecipeId).NotEmpty();
+        }
+    }
+}
