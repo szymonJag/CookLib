2a93bf3748aaa4b185b586a2a4d199cdb2235f81
Szymon <szymonjag2@gmail.com>
2023-04-04 10:31:44 +0200

    added validatio for update recipe

--- CookLib.ApplicationServices/API/Domain/Validators/Recipes/UpdateRecipeRequestValidator.cs
+++ CookLib.ApplicationServices/API/Domain/Validators/Recipes/UpdateRecipeRequestValidator.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CookLib.ApplicationServices.API.Domain.Requests.Recipes;
+using FluentValidation;
 
 namespace CookLib.ApplicationServices.API.Domain.Validators.Recipes
 {
-    public class UpdateRecipeRequestValidator
+    public class UpdateRecipeRequestValidator : AbstractValidator<UpdateRecipeByIdRequest>
     {
+        public UpdateRecipeRequestValidator()
+        {
+            RuleFor(x => x.Name).Length(4, 60);
+            RuleFor(x => x.PreparationTime).InclusiveBetween(1, 999);
+            RuleFor(x => x.ServingSize).InclusiveBetween(1, 999);
+        }
     }
 }
