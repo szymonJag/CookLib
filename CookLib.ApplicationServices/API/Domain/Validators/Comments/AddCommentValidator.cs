5978c5a565c6195e5d0ba230d95c87c50e83c092
Szymon <szymonjag2@gmail.com>
2023-04-04 10:48:20 +0200

    added validation for adding and updating comments

---
+++ CookLib.ApplicationServices/API/Domain/Validators/Comments/AddCommentValidator.cs
@@ -0,0 +1,15 @@
+﻿using CookLib.ApplicationServices.API.Domain.Requests.Comments;
+using FluentValidation;
+
+namespace CookLib.ApplicationServices.API.Domain.Validators.Comments
+{
+    public class AddCommentValidator : AbstractValidator<AddCommentRequest>
+    {
+        public AddCommentValidator()
+        {
+            RuleFor(x => x.AuthorId).NotEmpty();
+            RuleFor(x => x.RecipeId).NotEmpty();
+            RuleFor(x => x.Description).Length(1, 300);
+        }
+    }
+}
---
+++ CookLib.ApplicationServices/API/Domain/Validators/Comments/UpdateCommentValidator.cs
@@ -0,0 +1,15 @@
+﻿using CookLib.ApplicationServices.API.Domain.Requests.Comments;
+using FluentValidation;
+
+namespace CookLib.ApplicationServices.API.Domain.Validators.Comments
+{
+    public class UpdateCommentValidator : AbstractValidator<UpdateCommentByIdRequest>
+    {
+        public UpdateCommentValidator()
+        {
+            RuleFor(x => x.AuthorId).NotEmpty();
+            RuleFor(x => x.RecipeId).NotEmpty();
+            RuleFor(x => x.Description).Length(1, 300);
+        }
+    }
+}
